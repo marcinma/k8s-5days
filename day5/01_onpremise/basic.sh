0ada697bdb989bbc4b19faae155987894370b076
Marcin Makowski <ceo.nobilite@gmail.com>
2021-06-24 18:31:22 +0200

    base

---
+++ day5/01_onpremise/basic.sh
@@ -0,0 +1,34 @@
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common  openssh-server -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt install docker.io -y
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+
+sudo systemctl enable docker.service
+sudo systemctl restart docker.service
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt install -y kubelet kubeadm kubectl
